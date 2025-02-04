b600b554e36e10fda18a29884d12ecd90cfb8a30
Nitish Chopra <nitishchopra27@users.noreply.github.com>
2025-02-05 05:06:08 +1100

    first update - should not include one.py

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Program make a simple calculator
 
 # This function adds two numbers
@@ -15,3 +16,15 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
     return (x / y)
+
+a = add(5 ,6)
+print("Result of addition is: ", a)
+
+b = subtract(6, 5)
+print("Result of subtraction is: ", b)
+
+c = multiply(5, 6)
+print("Result of multiplication is: ", c)
+
+d = divide(10, 5)
+print("Result of division is: ", d)
